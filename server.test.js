d6405eb205e28fb6125d73cf1dad679320330c1a
Jesse Jackman <jessej20@nmsu.edu>
2022-11-04 02:12:32 -0600

    Update to backend. More tests, openAPI, and better endpoints.

--- server.test.js
+++ server.test.js
@@ -3,13 +3,19 @@ const app = require('./server.js');
 
 test('Get details about a tutor', async () => {
     await request(app)
-        .get('/api/tutorDetails/1')
+        .get('/api/tutorDetails/493458463')
         .expect(200);
 });
 
+ test('Testing for 400 when input for aggieId is a string', async () => {
+     await request(app)
+         .get('/api/tutorDetails/GIVEME400')
+         .expect(400);
+ });
+
 test('Get details about a student', async () => {
     await request(app)
-        .get('/api/studentDetails/1')
+        .get('/api/studentDetails/478214179')
         .expect(200);
 });
 
@@ -29,5 +35,17 @@ test('Get back home page', async () => {
 test('Get back all users', async () => {
     await request(app)
         .get('/api/userDetails/478214179')
+        .expect(200);
+});
+
+test('Get back all schedules', async () => {
+    await request(app)
+        .get('/api/listSchedules')
+        .expect(200);
+});
+
+test('Get back all bookings from a specific student', async () => {
+    await request(app)
+        .get('/api/listBookings/478214179')
         .expect(200);
 });
