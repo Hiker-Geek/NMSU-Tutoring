b9fc0a9cb5016a4f2b0c97248bba032cbe1e222b
fernadosali <salifern@nmsu.edu>
2022-11-11 10:27:54 -0500

    chore: update and delete queries

--- query.js
+++ query.js
@@ -2,6 +2,7 @@
 const sqlite3 = require("sqlite3").verbose();
 const axios = require("axios");
 const { response } = require("express");
+const { run } = require("jest");
 const db = new sqlite3.Database('./mock.db', sqlite3.OPEN_READWRITE, (err)=>{
     if (err) return console.error(err.message);
 
@@ -18,6 +19,7 @@ axios.get("https://random-data-api.com/api/users/random_user")
         const {email} = data;
         const {gender} = data;
 
+    //CREATE AN ENTRY 
     //     const sql = 'INSERT INTO student(first_name, last_name, username, date_of_birth, email, gender) VALUES(?,?,?,?,?,?)';
     //     db.run(sql, [first_name, last_name, username, date_of_birth, email,gender], (err)=>{
     //         if (err) return console.error(err.message);
@@ -30,19 +32,26 @@ axios.get("https://random-data-api.com/api/users/random_user")
         });
 
     
+//OUTPUT ENTRIES 
+const sql = 'SELECT * FROM student';
+db.all(sql, [], (err,rows)=>{
+    if (err) return console.error(err.message);
+    rows.forEach(row=>{
+        console.log(row);
+    });
+});
 
-// const sql = 'SELECT * FROM student';
-// db.all(sql, [], (err,rows)=>{
+// UPDATE AN ENTRY 
+// const sql = 'UPDATE student SET first_name = ? WHERE gender = ?';
+// db.run(sql, ['Ricky', 'Male'], function(err){
 //     if (err) return console.error(err.message);
-
-//     rows.forEach(row=>{
-//         console.log(row);
-//     });
 // });
 
-
-
-
+//DELETE AN ENTRY 
+// const sql = 'DELETE FROM student WHERE gender = ?';
+// db.run(sql, 'Male', function(err){
+//      if (err) return console.error(err.message);
+// });
 
 db.close((err)=>{
     if (err) return console.error(err.message);
