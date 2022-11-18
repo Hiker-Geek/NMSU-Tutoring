b8abfdae333091bd1a60da6f7a08e69595f88cea
Hiker-Geek <stein.jessicarose@gmail.com>
2022-11-18 04:05:42 -0700

    feat: search page complete

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 export {default as Layout} from './layout';
 export {default as Header} from './header';
 export {default as MyCalendar} from './myCalendar';
+export {default as DropdownBtn} from './searchComponents';
+export {default as Cards} from './searchComponents';
+
 
